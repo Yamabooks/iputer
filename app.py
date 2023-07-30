cd6d8d53de1d945336a72f5de8e5fe6cb3be0152
Yamabooks <136438115+Yamabooks@users.noreply.github.com>
2023-07-30 21:47:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-pip install -r requirements.txt
+from setuptools import setup, find_packages
+
+def load_requires_from_file(filepath):
+    with open(filepath) as fp:
+        return [pkg_name.strip() for pkg_name in fp.readlines()]
+
+setup(
+    # その他の項目は省略
+    install_requires=load_requires_from_file("requirements.txt")
+)
+
 
 import streamlit as st
 import random
